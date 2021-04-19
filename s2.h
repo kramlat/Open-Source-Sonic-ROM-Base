7cf1bd4d968a41ddf2a293b663b7f316702bbac8
Kramlat <kram2048@gmail.com>
2021-04-18 22:15:55 -0600

    initial commit

---
+++ s2.h
@@ -0,0 +1,7 @@
+comp_z80_size 0xF50 e für C-Programm */
+#define word_728C_user 0x87DC
+#define Obj5F_MapUnc_7240 0x885C
+#define off_3A294 0x3E334
+#define MapRUnc_Sonic 0x7D000
+#define movewZ80CompSize 0x11604E
+/* Ende Includefile für C-Programm */
